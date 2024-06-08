0ac5ddf889f358ce5ecf37c73268935d47fb827a
unknown <pickup.lee@gmail.com>
2024-06-08 17:44:57 +0800

    modified:   SQL_Project/Parser/sql_util/__pycache__/sql_analyzer.cpython-310.pyc 	modified:   SQL_Project/Parser/sql_util/sql_analyzer.py 	modified:   SQL_Project/Parser/sql_views/__pycache__/travel_sql_views.cpython-310.pyc 	modified:   SQL_Project/Parser/sql_views/travel_sql_views.py

--- SQL_Parser/SQL_Project/Parser/sql_views/travel_sql_views.py
+++ SQL_Parser/SQL_Project/Parser/sql_views/travel_sql_views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from Parser.sql_util import sql_analyzer
 import traceback
 
+
 # SET 不能直接Json 化  转换成 ARRAY
 def Trans_Set_To_Array(Src_Dict):
     rtn_array=[]
@@ -13,7 +14,6 @@ def Trans_Set_To_Array(Src_Dict):
         rtn_array.append({'TBL_NAME':buf_t,'COLS':list(Src_Dict[buf_t])})
     return rtn_array
 
-# 读取潜水者的总数已经信息列表
 @csrf_exempt
 def get_sql_info_ajax(request):
     @portal_check
@@ -21,10 +21,13 @@ def get_sql_info_ajax(request):
         # "SELECT  SUM(x.aaa) as sumA ,x.bbb AS fstC ,(tbl1.ccc+tbl2.ttt)*tbl3.www, tbl2.ddd ,tbl3.ggg FROM tbl0 x  JOIN tbl1  JOIN tbl2 JOIN tbl3  WHERE x.caa=2 and x.cbb=tbl1.ccc and tbl1.cee=(select sum(abc) from tbl3 where eee=199) and x.cff = (select a4 from tbl4 where id4=123 ) group by x.fff order by x.rrr"
         try:
             src_sql = params['sql_content']
-            final_map = sql_analyzer.rereat_info_from_sql(src_sql)
-            final_map['TBL'] = list(final_map['TBL']) 
+            final_dict = sql_analyzer.rereat_info_from_sql(src_sql)
+            table_list = list(final_dict.ref_tbl_set)
+            criteria_dict = Trans_Set_To_Array(final_dict.criteria_col_dict) 
+            out_dict = Trans_Set_To_Array(final_dict.out_col_dict)
+
         except Exception as err:
-            traceback.print_exc()
-            return JsonResponse({'AJAX_RESULT':False,'ERR_MSG':e.__str__()})
-        return JsonResponse({'AJAX_RESULT': True, 'RESULT': {'TBL':final_map['TBL'],'SRC':Trans_Set_To_Array(final_map['SRC']),'OUT':Trans_Set_To_Array(final_map['OUT'])}})
+            return JsonResponse({'AJAX_RESULT':False,'ERR_MSG':str(err)})
+        return JsonResponse({'AJAX_RESULT': True, 'RESULT': {'TBL':table_list,
+        'SRC':criteria_dict,'OUT':out_dict}})
     return biz_view(request)
